6de06ed17d3018453759590a3f29daad417d0470
maksyliator <maksim.tankov@gmail.com>
2021-04-28 10:18:29 +0300

    fix fix game launch

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Brain\Games\Cli;
-
 use function cli\line;
 use function cli\prompt;
 
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+function sayHello()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
