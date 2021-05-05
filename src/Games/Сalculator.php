f64f4ab7153f6029856526feeee30b38e5f4313a
maksyliator <maksim.tankov@gmail.com>
2021-05-05 16:29:57 +0300

    Add Calculator.php

--- src/Games/Сalculator.php
+++ src/Games/Сalculator.php
@@ -1,1 +1,41 @@
 <?php
+
+namespace Brain\Calc\Calculator;
+
+use function cli\line;
+use function cli\prompt;
+
+function calculator()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('What is the result of the expression?');
+    for ($i = 1; $i <= 3; $i++) {
+        $number1 = rand(1, 20);
+        $number2 = rand(1, 20);
+        $determinantOfArithmeticExpression = rand(1, 3);
+        switch ($determinantOfArithmeticExpression) {
+            case 1:
+                $result = $number1 + $number2;
+                $question = "{$number1} + {$number2}";
+                break;
+            case 2:
+                $result = $number1 - $number2;
+                $question = "{$number1} - {$number2}";
+                break;
+            case 3:
+                $result = $number1 * $number2;
+                $question = "{$number1} * {$number2}";
+                break;
+        }
+        line('Question: %s', $question);
+        $answer = prompt('Your answer');
+        if ($result === (int)$answer) {
+            line('Correct!');
+        } else {
+            return line("'{$answer}' is wrong answer ;(. Correct answer was '{$result}'.\nLet's try again, %s!", $name);
+        }
+    }
+    return line("Congratulations, %s!", $name);
+}
