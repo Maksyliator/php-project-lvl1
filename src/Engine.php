77cba38fed75347d9d76bfef58a2ee2d49f1e640
maksyliator <maksim.tankov@gmail.com>
2021-05-05 19:54:58 +0300

    Create game logic

--- src/Engine.php
+++ src/Engine.php
@@ -1,2 +1,38 @@
 <?php
 
+namespace Brain\Games\Engine;
+
+use function cli\line;
+use function cli\prompt;
+use function Brain\Even\Paritycheck\parityCheck;
+use function Brain\Calc\Calculator\calculator;
+
+function gameLogic()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line("Select the game number.");
+    line("1. Parity check");
+    line("2. Calculator");
+    $gameNumber = prompt('Enter its number.');
+    for ($i = 1; $i <= 3; $i++) {
+        if ($gameNumber === '1') {
+            if ($i === 1) {
+                line('Answer "yes" if the number is even, otherwise answer "no".');
+            }
+            $result = parityCheck();
+        }
+        if ($gameNumber === '2') {
+            if ($i === 1) {
+                line('What is the result of the expression?');
+            }
+            $result = calculator();
+        }
+        line($result, "\n");
+        if ($result !== 'Correct!') {
+            return line("Let's try again, %s!", $name);
+        }
+    }
+    return line("Congratulations, %s!", $name);
+}
--- src/Games/Сalculator.php
+++ src/Games/Сalculator.php
@@ -7,35 +7,24 @@ use function cli\prompt;
 
 function calculator()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-    line('What is the result of the expression?');
-    for ($i = 1; $i <= 3; $i++) {
-        $number1 = rand(1, 20);
-        $number2 = rand(1, 20);
-        $determinantOfArithmeticExpression = rand(1, 3);
-        switch ($determinantOfArithmeticExpression) {
-            case 1:
-                $result = $number1 + $number2;
-                $question = "{$number1} + {$number2}";
-                break;
-            case 2:
-                $result = $number1 - $number2;
-                $question = "{$number1} - {$number2}";
-                break;
-            case 3:
-                $result = $number1 * $number2;
-                $question = "{$number1} * {$number2}";
-                break;
-        }
-        line('Question: %s', $question);
-        $answer = prompt('Your answer');
-        if ($result === (int)$answer) {
-            line('Correct!');
-        } else {
-            return line("'{$answer}' is wrong answer ;(. Correct answer was '{$result}'.\nLet's try again, %s!", $name);
-        }
+    $number1 = rand(1, 20);
+    $number2 = rand(1, 20);
+    $determinantOfArithmeticExpression = rand(1, 3);
+    switch ($determinantOfArithmeticExpression) {
+        case 1:
+            $result = $number1 + $number2;
+            $question = "{$number1} + {$number2}";
+            break;
+        case 2:
+            $result = $number1 - $number2;
+            $question = "{$number1} - {$number2}";
+            break;
+        case 3:
+            $result = $number1 * $number2;
+            $question = "{$number1} * {$number2}";
+            break;
     }
-    return line("Congratulations, %s!", $name);
+    line('Question: %s', $question);
+    $answer = prompt('Your answer');
+    return $result === (int)$answer ? 'Correct!' : "'{$answer}' is wrong answer ;(. Correct answer was '{$result}'.";
 }
