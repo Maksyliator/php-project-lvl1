1a75e710c816c38a4d5245d9b4d77643ffc52ca1
maksyliator <maksim.tankov@gmail.com>
2021-05-07 21:34:19 +0300

    Update games progression

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -1,3 +1,23 @@
 <?php
 
 namespace Brain\Progression\Progression;
+
+use function cli\line;
+use function cli\prompt;
+
+function progression()
+{
+    $progressionStep = rand(1, 10);
+    $firstNumberOfProgression = rand(1, 100);
+    $itemNumberForQuestion = rand(0, 9);
+    $progression[0] = $firstNumberOfProgression;
+    $question = $progression[0];
+    for ($i = 1; $i <= 9; $i++) {
+        $progression[$i] = $progression[$i - 1] + $progressionStep;
+        $itemNumberForQuestion !== $i ? $question .= " {$progression[$i]}" : $question .= ' ..';
+    }
+    line('Question: %s', $question);
+    $answer = prompt('Your answer');
+    return (int)$answer === $progression[$itemNumberForQuestion] ? 'Correct!' :
+        "'{$answer}' is wrong answer ;(. Correct answer was '{$progression[$itemNumberForQuestion]}'.";
+}
