a4e9045782f16dfac18e08c257a8d2fd8fb608c3
maksyliator <maksim.tankov@gmail.com>
2021-05-25 19:01:20 +0300

    fix

--- src/Games/Calculator.php
+++ src/Games/Calculator.php
@@ -31,7 +31,7 @@ function getARandomMathExpression(): array
 
 function calculate(int $number1, int $number2, string $randomMathematicalOperator): int
 {
-    $result = null;
+    $result = 0;
     switch ($randomMathematicalOperator) {
         case '+':
             $result = $number1 + $number2;
