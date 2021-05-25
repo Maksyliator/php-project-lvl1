529297e15a240d3533b8a33278bf9728e6992da5
maksyliator <maksim.tankov@gmail.com>
2021-05-25 18:51:10 +0300

    Assign to variable to function 'calculate' '$result' value 'null'

--- src/Games/Calculator.php
+++ src/Games/Calculator.php
@@ -31,7 +31,7 @@ function getARandomMathExpression(): array
 
 function calculate(int $number1, int $number2, string $randomMathematicalOperator): int
 {
-    $result = 0;
+    $result = null;
     switch ($randomMathematicalOperator) {
         case '+':
             $result = $number1 + $number2;
