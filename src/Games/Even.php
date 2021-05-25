b9ff82304377f6704af71edecdc207c534ef3e35
maksyliator <maksim.tankov@gmail.com>
2021-05-25 17:56:45 +0300

    Add function is Even

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -11,12 +11,16 @@ const RULE_OF_GAME = 'Answer "yes" if the number is even, otherwise answer "no".
 function playParityCheck(): void
 {
     $question = [];
-    $parityCheck = [];
     $result = [];
     for ($i = 1; $i <= NUMBER_OF_ROUND; $i++) {
         $question[$i] = rand(1, 100);
-        $parityCheck[$i] = $question[$i] % 2;
-        $parityCheck[$i] === 0 ? $result[$i] = 'yes' : $result[$i] = 'no';
+        isEven($question[$i]) ? $result[$i] = 'yes' : $result[$i] = 'no';
     }
     useGameLogic(RULE_OF_GAME, $question, $result);
+}
+
+function isEven(int $number): bool
+{
+    $parityCheck = $number % 2;
+    return $parityCheck === 0 ?: false;
 }
